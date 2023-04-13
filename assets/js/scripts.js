06f2a5845132a294ecef9b7ec01d1a342518ac79
Wermison Sousa <wermison_15@hotmail.com>
2023-04-12 21:31:26 -0300

    Calculo de area de 3 lados concluido

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+let lado1 = document.querySelector('.lado1');
+let lado2 = document.querySelector('.lado2');
+let lado3 = document.querySelector('.altura');
+
+document.querySelector('#calcular button').addEventListener('click', () => {
+    let comprimento = (Number(lado1.value) + Number(lado2.value)) / 2;
+    let altura = Number(lado3.value);
+    let area = comprimento * altura;
+    let ha = area / 10000;
+
+    if (lado1.value <= 0 || lado2.value <= 0 || lado3.value <= 0) {
+        document.querySelector('.result').innerHTML = ``;
+        document.querySelector('.result').innerHTML = `<p class="alert alert-danger text-center">Preencha as informações com valores numéricos positivos</p>`;
+    } else {
+        document.querySelector('.result').innerHTML = ``;
+        document.querySelector('.result').innerHTML += `<p class="alert alert-success text-center">O imóvel tem <b>${area.toLocaleString()}</b>m<sup>2</sup> ou <b>${ha.toLocaleString()}</b>ha</p>`;
+    }
+
+
+});
